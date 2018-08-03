36a21118317a6e9f06ce7f3964e26d0a47767a32
superrabbitrun007` <244398764@qq.com>
2018-08-03 21:03:32 +0800

    点击列表可以显示对应的图片，图片点击计数正常 	待完成：css 响应式页面（特别注意不同大小的原始图片，如何正常自适应）

--- js/app1.js
+++ js/app1.js
@@ -47,6 +47,8 @@ function click11() {
 function sidebarClick() {
 
 
+
+
 }
 
 
@@ -67,54 +69,71 @@ for (var i = 0; i < cats.length; i++) {
 	if(i === 4) {
 		side1[0].remove();
 	}
+
+	eleSide.addEventListener('click', (function(index) {
+		return function() {
+			console.log("test sidebar");
+			catClick(index);
+		};
+
+	})(i));
+
 }
 
 
 
+// 添加每张图片并且监听
+
+function catClick(i) {
+
+
+    // for (var i = 0; i < cats.length; i++) {
+
+        let cat = cats[i];
+        // console.log(cat);
+        //将列表中每只猫的innerHTML赋值给catInner
+        let catInner = cat[0].innerHTML;
+        //每只猫重命名
+        let catName = ("cat" + (i + 1));
+        //新建div,包裹每只猫的代码
+        let elem = document.createElement('div');
+        elem.innerHTML = catInner;
+        //从第二只猫开始，所以class从cat2开始
+        elem.classList.add('cat' + (i + 1));
+        // let a = elem;
+        // console.log(a);
+        // console.log(elem.children[0]);
+        elem.children[0].textContent = catName;
+        //将图片的地址放在列表中，根据对应的i， 加载不同的图片，写入innerHTML中
+        elem.children[1].outerHTML = catsSrc[i];
+
+        // console.log(elem);
+
+        // console.log(elem.children);
+        let count = 0;
+        elem.addEventListener('click', (function(catDiv, countN) {
+            return function() {
+                countN += 1;
+                console.log(countN);
+                console.log(catDiv);
+                // catDiv.children[1].innerText = catName;
+
+                catDiv.children[2].textContent = 'Total clicks is ' + countN + '.';
+
+            };
+        })(elem, count));
+        let catContent = document.getElementsByClassName('cat');
+        // console.log(catContent);
+        //添加图片前，先清空div中的图片信息（避免界面中显示多张图片）
+        catContent[0].innerHTML = "";
+        catContent[0].appendChild(elem);
+        //当添加完5张图片后，删除原始的图片(如果下面的条件中i<4时，就删除原始图片，则会报错，图片加载不完)
+        // if (i === 4) {
+        //     let el = document.querySelector('.cat0');
+        //     el.remove();
+        // }
+    // }
 
 
 
-
-for (var i = 0; i < cats.length; i++) {
-
-	let cat = cats[i];
-	// console.log(cat);
-	//将列表中每只猫的innerHTML赋值给catInner
-	let catInner = cat[0].innerHTML;
-	//每只猫重命名
-	let catName = ("cat" + (i+1));
-	//新建div,包裹每只猫的代码
-	let elem = document.createElement('div');
-	elem.innerHTML = catInner;
-	//从第二只猫开始，所以class从cat2开始
-	elem.classList.add('cat'+(i+1));
-	// let a = elem;
-	// console.log(a);
-	// console.log(elem.children[0]);
-	elem.children[0].textContent = catName ;
-	//将图片的地址放在列表中，根据对应的i， 加载不同的图片，写入innerHTML中
-	elem.children[1].outerHTML = catsSrc[i];
-
-	// console.log(elem);
-	
-	// console.log(elem.children);
-	let count = 0;
-	elem.addEventListener('click', (function(catDiv,countN) {
-		return function() {
-			countN += 1;
-			console.log(countN);
-			console.log(catDiv);
-			// catDiv.children[1].innerText = catName;
-
-			catDiv.children[2].textContent = 'Total clicks is ' + countN +'.';
-			
-		};
-	})(elem,count));
-
-	document.getElementsByClassName('cat')[0].appendChild(elem);
-	//当添加完5张图片后，删除原始的图片(如果下面的条件中i<4时，就删除原始图片，则会报错，图片加载不完)
-	if(i === 4) {
-		let el = document.querySelector('.cat0');
-		el.remove();
-	}
 }
