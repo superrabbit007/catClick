4be709d3f12d561455b18b4d3efc9b408c91aff9
superrabbitrun007` <244398764@qq.com>
2018-08-03 16:44:42 +0800

    添加5张不同的猫，可以分别点击计数

--- js/app1.js
+++ js/app1.js
@@ -3,32 +3,50 @@
 
 
 let cat1 = document.getElementsByClassName('cat0');
-// document.body.innerHTML = '';
 console.log(cat1);
 let cat1Text = cat1[0].outerHTML;
 let cat2Text = cat1Text;
-let cat3 = cat2 = cat1;
+let cat5 = cat4 = cat3 = cat2 = cat1;
 
-let cats = [cat1, cat2, cat3];
+let cats = [cat1,cat2, cat3, cat4, cat5];
+// document.body.innerHTML = '';
+
+
+src1 = '<img class = "my-elem" src = "img/cat1.jpg">';
+src2 = '<img class = "my-elem" src = "img/cat2.jpg">';
+src3 = '<img class = "my-elem" src = "img/cat3.jpg">';
+src4 = '<img class = "my-elem" src = "img/cat4.jpg">';
+src5 = '<img class = "my-elem" src = "img/cat5.jpg">';
+
+let catsSrc = [src1,src2, src3, src4, src5];
+
+
+
+
 
 for (var i = 0; i < cats.length; i++) {
+
 	let cat = cats[i];
-	// cat[0].classList.add('cat'+(i+1));
-	let cat111 = cat[0].innerHTML;
-	let catName = "cat" + i ;
+	console.log(cat);
+	//将列表中每只猫的innerHTML赋值给catInner
+	let catInner = cat[0].innerHTML;
+	//每只猫重命名
+	let catName = ("cat" + (i+1));
+	//新建div,包裹每只猫的代码
 	let elem = document.createElement('div');
-	elem.innerHTML = cat111;
+	elem.innerHTML = catInner;
+	//从第二只猫开始，所以class从cat2开始
 	elem.classList.add('cat'+(i+1));
-	let a = elem;
-	console.log(a);
+	// let a = elem;
+	// console.log(a);
 	console.log(elem.children[0]);
-	elem.children[0].innerHTML = "cat" + (i+1) ;
-	elem.children[1];
+	elem.children[0].innerHTML = catName ;
 	//将图片的地址放在列表中，根据对应的i， 加载不同的图片，写入innerHTML中
+	elem.children[1].outerHTML = catsSrc[i];
 
-
+	console.log(elem);
 	
-	console.log(elem.children);
+	// console.log(elem.children);
 	let count = 0;
 	elem.addEventListener('click', (function(catDiv,countN) {
 		return function() {
@@ -43,4 +61,9 @@ for (var i = 0; i < cats.length; i++) {
 	})(elem,count));
 
 	document.getElementsByClassName('cat')[0].appendChild(elem);
+	//当添加完5张图片后，删除原始的图片(如果下面的条件中i<4时，就删除原始图片，则会报错，图片加载不完)
+	if(i === 4) {
+		let el = document.querySelector('.cat0');
+		el.remove();
+	}
 }
